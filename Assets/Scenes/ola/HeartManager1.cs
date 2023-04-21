c2ced5aaf136aa2efedf471c0c705b12d20c3c6d
Kosmo n301 <mkosmalski2@edu.cdv.pl>
2023-04-21 17:12:28 +0200

    Folder cleanup

---
+++ Assets/Scenes/ola/HeartManager1.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartManager1 : MonoBehaviour
+{
+    public int health = 3;
+    public Image[] hearts;
+
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+
+    void Update()
+    {
+        foreach (Image img in hearts)
+        {
+            img.sprite = emptyHeart;
+        }
+        for (int i = 0; i < health; i++)
+        {
+            hearts[i].sprite = fullHeart;
+        }
+
+    }
+}
