992b9d7de91304243486170a59ca00c9c44aab57
Kosmo <error.mk85@gmail.com>
2023-05-04 14:15:39 +0200

    Menu scene update

--- Assets/Scenes/ola/menu.cs
+++ Assets/Scenes/ola/menu.cs
@@ -7,7 +7,7 @@ public class menu : MonoBehaviour
 {
     public void FirstScene()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(2);
      
     }
     public void Exit()
