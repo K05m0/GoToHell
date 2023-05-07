62a5179c8d9e2f3c749014b8d6c741c5b3cbb530
Aleksandra <achudy@edu.cdv.pl>
2023-05-07 15:59:38 +0200

    update health death

--- Assets/Scenes/ola/DamageControler.cs
+++ Assets/Scenes/ola/DamageControler.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class DamageControler : MonoBehaviour
 {
+    public GameEvent onPlayerdamage;
     public playerhealth Hp;
+
+    public GameEvent onPlayerdeath;
+
+   
+
     private void OnMouseDown()
     {
         Hp.value -= 1;
+        if (onPlayerdamage != null)
+            onPlayerdamage.Fire();
     }
     
 }
