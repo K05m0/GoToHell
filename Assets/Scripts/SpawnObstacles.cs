7d5a41d2b7336103575cb15750cc63fce07f567a
K05m0 <error.mk85@gmail.com>
2023-05-29 20:21:33 +0200

    Enemy Spawning System

--- Assets/Scripts/SpawnObstacles.cs
+++ Assets/Scripts/SpawnObstacles.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class SpawnObstacles : PlayerMovement
+public class SpawnObstacles : MonoBehaviour
 {
     private float startDistance = 10;
     private float yDistance = 5;
     private float WebDistance = 25;
     private float TreeDistance = 8;
+    private float WallDistance = 35;
+    private float FlyingDistance = 25;
     private float minSpread = 5;
     private float maxSpread = 10;
     private float spawnTreshold = -10;
@@ -16,15 +18,19 @@ public class SpawnObstacles : PlayerMovement
     private float lastYPositionWeb;
     private float lastYPositionBarrier;
     private float lastYPositionTree;
-    
+    private float lastYPositionFlying;
+    private float lastYPositionWall;
+
 
 
     public Transform playerTransform;
     public Transform obstaclePrefab;
     public Transform webPrefab;
     public Transform TreePrefab;
+    public Transform FlyingEnemy;
+    public Transform WallEnemy;
 
-    public float ySpread = 25;
+    public float ySpread = 35;
     float lastYPos;
 
     void Start()
@@ -33,6 +39,8 @@ public class SpawnObstacles : PlayerMovement
         lastYPositionWeb = playerTransform.position.y;
         lastYPositionBarrier = playerTransform.position.y;
         lastYPositionTree = playerTransform.position.y;
+        lastYPositionFlying = playerTransform.position.y;
+        lastYPositionWall = playerTransform.position.y;
     }
 
 
@@ -43,6 +51,63 @@ public class SpawnObstacles : PlayerMovement
 
     void Update()
     {
+        if (lastYPositionWall - playerTransform.position.y > WallDistance)
+        {
+            float side = Random.Range(0, 10);
+            if (side <= 5)
+            {
+                float WallPosx = -11;
+                float WallPosZ = 0;
+                Instantiate(WallEnemy, new Vector3(WallPosx, playerTransform.position.y - ySpread, WallPosZ), Quaternion.identity);
+                lastYPositionWall = playerTransform.position.y;
+                return;
+            }
+            else
+            {
+                float WallPosx = 11;
+                float WallPosZ = 0;
+                Instantiate(WallEnemy, new Vector3(WallPosx, playerTransform.position.y - ySpread, WallPosZ), Quaternion.identity);
+                lastYPositionWall = playerTransform.position.y;
+                return;
+            }
+        }
+
+
+        if (lastYPositionFlying - playerTransform.position.y > FlyingDistance)
+        {
+            float side = Random.Range(0, 10);
+            if (side <= 5)
+            {
+                float FlyingPosx = Random.Range(10, -10);
+                float FlyingPosZ = 0;
+                Instantiate(FlyingEnemy, new Vector3(FlyingPosx, playerTransform.position.y - ySpread, FlyingPosZ), Quaternion.identity);
+                lastYPositionFlying = playerTransform.position.y;
+                return;
+            }
+            else
+            {
+                float FlyingPosx = Random.Range(10, -10);
+                float FlyingPosZ = 0;
+                Instantiate(FlyingEnemy, new Vector3(FlyingPosx, playerTransform.position.y - ySpread, FlyingPosZ), Quaternion.identity);
+                lastYPositionFlying = playerTransform.position.y;
+                return;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         if (lastYPositionTree - playerTransform.position.y > TreeDistance)
         {
             float side = Random.Range(0, 10);
@@ -52,6 +117,7 @@ public class SpawnObstacles : PlayerMovement
                 float TreePosZ = Random.Range(-7, -13);
                 Instantiate(TreePrefab, new Vector3(TreePosx, playerTransform.position.y - ySpread, TreePosZ), Quaternion.identity);
                 lastYPositionTree = playerTransform.position.y;
+                return;
             }
             else
             {
@@ -59,6 +125,7 @@ public class SpawnObstacles : PlayerMovement
                 float TreePosZ = Random.Range(-7, -13);
                 Instantiate(TreePrefab, new Vector3(TreePosx, playerTransform.position.y - ySpread, TreePosZ), Quaternion.identity);
                 lastYPositionTree = playerTransform.position.y;
+                return;
             }
             
         }
