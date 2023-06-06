61f251daf9267357264e0ad0f9255b7413fbc016
Sanki <michal.sanki@gmail.com>
2023-06-07 00:37:56 +0200

    Improved Dashing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,8 +10,7 @@ public class PlayerMovement : StaminaBar
     public Vector3Value positionValue;
     
     public Camera camera2;
-    public float tapSpeed = 0.5f;
-    private float lastTapTime = 0;
+   
 
     public float maxSpeed;
     public float maxSpeedIncrementRate = 0.1f; // Rate at which the max speed increases per second
@@ -26,15 +25,10 @@ public class PlayerMovement : StaminaBar
     public int ammoCount = 999;
 
     [SerializeField] private Transform PlayerCamera;
-    [SerializeField] private Rigidbody PlayerBody;
-    [SerializeField] private float Speed;
-
+    [SerializeField] private float MovingSpeed;
     [SerializeField] private float JumpForce;
     [SerializeField] private float DashForce;
     [SerializeField] private float Velocity;
-
-    [SerializeField] private float wallride;
-    [SerializeField] public float kickbackForce;
 
     public GameObject bullet;
     public Transform bulletTransform;
@@ -51,16 +45,13 @@ public class PlayerMovement : StaminaBar
 
     private void Start()
     {
-        lastTapTime = 0;
+       
         camera2 = Camera.main;
         mainBurst.Stop();
         secondaryBurst.Stop();
     }
 
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+    
 
     private void Update()
     {
@@ -71,14 +62,15 @@ public class PlayerMovement : StaminaBar
         Vector2 mouseOnScreen = (Vector2)camera2.ScreenToViewportPoint(Input.mousePosition);
         Vector2 mouseOnScreenScaled = positionOnScreen - mouseOnScreen;
 
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            if ((Time.time - lastTapTime) < tapSpeed && staminaBar.value >= DashCost)
+            if ( staminaBar.value >= DashCost)
             {
-                PlayerBody.AddRelativeForce((GameObject.FindGameObjectWithTag("BulletTransform").transform.position - transform.position) * DashForce, ForceMode.VelocityChange);
+                rb.velocity = Vector3.zero;
+                rb.AddRelativeForce((GameObject.FindGameObjectWithTag("BulletTransform").transform.position - transform.position) * DashForce, ForceMode.VelocityChange);
                 StaminaBar.instance.UseStamina(DashCost);
             }
-            lastTapTime = Time.time;
+            
         }
 
         if (!canFire)
@@ -98,12 +90,11 @@ public class PlayerMovement : StaminaBar
             ammoCount--;
             Debug.Log("Ammo Count" + ammoCount);
 
-            Vector3 kickbackDirection = -transform.forward; // Adjust the kickback direction as needed
-            ApplyKickbackForce(kickbackDirection * kickbackForce);
+           
         }
 
         horizontal = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(horizontal * Speed, rb.velocity.y);
+        rb.velocity = new Vector2(horizontal * MovingSpeed, rb.velocity.y);
 
         if (Input.GetKeyDown(KeyCode.Space) && staminaBar.value >= JumpCost)
         {
@@ -120,10 +111,7 @@ public class PlayerMovement : StaminaBar
 
     }
 
-    public void ApplyKickbackForce(Vector3 force)
-    {
-        rb.AddForce(force, ForceMode.Impulse);
-    }
+ 
 
     private void PlayerJump()
     {
@@ -143,10 +131,7 @@ public class PlayerMovement : StaminaBar
 
     private void OnTriggerEnter(Collider collision)
     {
-        if (collision.gameObject.tag == "floor")
-        {
-            rb.velocity = Vector3.down * wallride;
-        }
+    
 
         if (collision.gameObject.tag == "enemy")
         {
