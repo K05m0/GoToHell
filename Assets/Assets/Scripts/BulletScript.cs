696381fa3847e8f2765a53f64ade2e88d594c4cb
Sanki <michal.sanki@gmail.com>
2023-06-06 20:44:21 +0200

    cleanup and cliffs

--- Assets/Assets/Scripts/BulletScript.cs
+++ Assets/Assets/Scripts/BulletScript.cs
@@ -17,8 +17,8 @@ public class BulletScript : MonoBehaviour
 
         playerMovement = FindObjectOfType<PlayerMovement>();
 
-        Vector3 kickbackDirection = -transform.forward; // Adjust the kickback direction as needed
-        playerMovement.ApplyKickbackForce(kickbackDirection * playerMovement.kickbackForce);
+       // Vector3 kickbackDirection = -transform.forward; // Adjust the kickback direction as needed
+      //  playerMovement.ApplyKickbackForce(kickbackDirection * playerMovement.kickbackForce);
     }
 
     private void Update()
